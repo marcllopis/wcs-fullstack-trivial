8160a1fe81e9ab7f8512b110a8f404921867d069
Marc <marcllopis89@gmail.com>
2022-03-22 11:25:43 +0100

    Adds a fn that modifies the html

--- index.js
+++ index.js
@@ -23,3 +23,29 @@ let questions = [
     correctAnswer: "New Zealand",
   },
 ];
+
+let trivial = document.getElementById("trivial-group");
+
+let index = 0;
+let points = 0;
+
+function generateTriviaHtml(arrayOfQuestions, index, currentPoints) {
+  let trivialToHtml = `
+  <h3>${arrayOfQuestions[index].question}</h3>
+  <div class="options">
+    <div class="row-options">
+      <button class="option-btn">${arrayOfQuestions[index].answers[0]}</button>
+      <button class="option-btn">${arrayOfQuestions[index].answers[1]}</button>
+    </div>
+    <div class="row-options">
+      <button class="option-btn">${arrayOfQuestions[index].answers[2]}</button>
+      <button class="option-btn">${arrayOfQuestions[index].answers[3]}</button>
+    </div>
+  </div>
+  <h4 class="points">${currentPoints} points</h4>
+`;
+
+  trivial.innerHTML = trivialToHtml;
+}
+
+generateTriviaHtml(questions, index, points);
