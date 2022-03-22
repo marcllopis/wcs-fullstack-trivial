3515258d47a43fc0004695d759ce7533ca4c7708
Marc <marcllopis89@gmail.com>
2022-03-22 10:57:53 +0100

    Adds question array of objects

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hey");
+// User selects an option and will get +10 points if the answer is correct and -3 if its not correct. After that, new question will appear until the end of all questions
+// At the end we show a game over message that says how much point they got
+
+let questions = [
+  {
+    question: "Who dreamed of a dream of somebody?",
+    answers: ["Marc", "Luke", "Arne", "Oerd"],
+    correctAnswer: "Oerd",
+  },
+  {
+    question: "Capital of Spain is?",
+    answers: ["Barcelona", "Madrid", "Sevilla", "Bilbao"],
+    correctAnswer: "Madrid",
+  },
+  {
+    question: "Marc dog name is?",
+    answers: ["Marc", "Luke", "Arne", "Oerd"],
+    correctAnswer: "Luke",
+  },
+  {
+    question: "Where did Julia lived? ",
+    answers: ["New Zealand", "Austria", "Barcelona", "Brazil"],
+    correctAnswer: "New Zealand",
+  },
+];
